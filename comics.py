af4d3883e861891610ba9f3a0c9df6a5ae6b4d83
Ash2803 <ashot.93@mail.ru>
2022-10-15 02:09:06 +0400

    Updated get_comics, added random comic downloading, fixed publish_wall_photo func

--- comics.py
+++ comics.py
@@ -1,16 +1,20 @@
 import os
-from pprint import pprint
+import random
 
 import requests
 from dotenv import load_dotenv
 
 
 def get_comics():
-    response = requests.get('https://xkcd.com/353/info.0.json')
+    current_comic = requests.get('https://xkcd.com/info.0.json')
+    current_comic.raise_for_status()
+    last_comic = current_comic.json()['num']
+    random_comic_num = random.randint(0, last_comic)
+    response = requests.get(f'https://xkcd.com/{random_comic_num}/info.0.json')
     response.raise_for_status()
     image = requests.get(response.json()['img'])
     image.raise_for_status()
-    comics_name = 'Comics.png'
+    comics_name = 'comic.png'
     with open(comics_name, 'wb') as file:
         file.write(image.content)
     return response.json()['alt']
@@ -40,7 +44,7 @@ def get_wall_upload_url(vk_token):
 
 
 def upload_comics(upload_url):
-    with open('Comics.png', 'rb') as file:
+    with open('comic.png', 'rb') as file:
         url = upload_url
         files = {
             'photo': file,
@@ -68,14 +72,13 @@ def save_wall_photo(vk_token, image):
     return image_info
 
 
-def publish_wall_photo(vk_token, owner_id, media_id, text):
+def publish_wall_photo(vk_token, group_id, owner_id, media_id, text):
     params = {
         'access_token': vk_token,
-        'group_id': '216491312',
         'from_group': 1,
         'v': 5.131,
         'attachments': f'photo{owner_id}_{media_id}',
-        'owner_id': owner_id,
+        'owner_id': f'-{group_id}',
         'message': text
     }
     response = requests.post('https://api.vk.com/method/wall.post', params=params)
@@ -86,13 +89,11 @@ def publish_wall_photo(vk_token, owner_id, media_id, text):
 def main():
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
-    photo_message = "I wrote 20 short programs in Python yesterday.  It was wonderful.  Perl, I'm leaving you."
-    # print(get_comics())
-    # # get_comics()
-    # # print(get_wall_upload_address(vk_token))
-    upload_photo = upload_comics(get_wall_upload_url(vk_token))
-    a = save_wall_photo(vk_token, upload_photo)
-    print(publish_wall_photo(vk_token, a['owner_id'], a['id'], photo_message))
+    group_id = '216491312'
+    get_comics()
+    uploaded_photo = upload_comics(get_wall_upload_url(vk_token))
+    saved_photo = save_wall_photo(vk_token, uploaded_photo)
+    print(publish_wall_photo(vk_token, group_id, saved_photo['owner_id'], saved_photo['id'], get_comics()))
 
 
 if __name__ == '__main__':
